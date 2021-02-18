65b41e43f9cbbb981e81b01709dc4a4aa935fd0f
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2021-02-18 12:30:44 +0700

    change 75

--- train.bat
+++ train.bat
@@ -1,1 +1,1 @@
-python train.py --dataset expanding --epochsave 1 --a 0.5 --commit --name expanding --bit 3 --batch_size 1 --debug
+python train.py --dataset expanding --epochsave 1 --a 0.75 --commit --name expanding --bit 3 --batch_size 1 --debug
