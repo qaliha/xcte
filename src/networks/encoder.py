91a1477f7767e62ff61a6e53586e58109f6c21ac
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2021-02-24 15:40:59 +0700

    change

--- src/networks/encoder.py
+++ src/networks/encoder.py
@@ -16,7 +16,7 @@ class FeatureExtractor(nn.Module):
         model = [ConvLayer(3, n_features, 3, 1, norm='skip')]
         model += [ConvLayer(n_features, n_features, 3, 1, norm='skip')]
         # Ok for now remove this and copy the reference networks
-        # model += [ConvLayer(n_features, n_features, 3, 1, norm='skip')]
+        model += [ConvLayer(n_features, n_features, 3, 1, norm='skip')]
         model += [ConvLayer(n_features, 12, 3, 2,
                             norm='skip', activation='skip')]
         model += [nn.PixelShuffle(2)]
