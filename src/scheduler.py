7b0169fce5706d6022b4ca188a8012c4d0b3845b
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2021-03-02 10:50:30 +0700

    add new scheduler

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,11 +1,16 @@
 from torch.optim import lr_scheduler
 
 
-def get_scheduler(optimizer, opt):
-    def lambda_rule(epoch):
-        lr_l = 1.0 - max(0, epoch - 2) / float(opt.nepoch - 2)
-        return lr_l
-    scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_rule)
+def get_scheduler(optimizer, opt, option='lambda', step_length=15):
+    scheduler = None
+    if option == 'lambda':
+        def lambda_rule(epoch):
+            lr_l = 1.0 - max(0, epoch - 2) / float(opt.nepoch - 2)
+            return lr_l
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_rule)
+    elif option == 'step':
+        scheduler = lr_scheduler.StepLR(
+            optimizer, step_size=step_length, gamma=0.1)
 
     return scheduler
 
