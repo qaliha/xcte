9492e65cfc3a6bf9d2cbcc26c116c091c95f1a55
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2021-03-30 18:02:49 +0700

    fix

--- train.bat
+++ train.bat
@@ -1,1 +1,1 @@
-python train.py --dataset expanding --epochsave 1 --lr 0.001 --a 2.0 --commit --name companding_GANs --bit 3 --epoch_count 1 --batch_size 1 --debug
+python train.py --optimized_encoder --dataset expanding --epochsave 1 --lr 0.001 --a 2.0 --commit --name companding_GANs --bit 2 --epoch_count 1 --batch_size 1 --debug
