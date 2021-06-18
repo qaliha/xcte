04d4f90c9050a23531cd88abac7d4b0ab82c270c
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2021-06-19 00:09:59 +0700

    change

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -110,3 +110,68 @@ def add_padding(tensor, p):
 
     tensor = F.pad(tensor, [0, padding_w, 0, padding_h], 'reflect')
     return tensor, h, w
+
+
+def mkdir(directory, mode=0o777):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        os.chmod(directory, mode=mode)
+
+
+def dir_exists(directory):
+    return os.path.exists(directory)
+
+
+def apply_threshold(value):
+    "Returns 0 or 255 depending where value is closer"
+    return 255 * math.floor(value/128)
+
+
+def sp_halftone(image_file):
+    pixel = image_file.load()
+
+    x_lim, y_lim = image_file.size
+
+    for y in range(1, y_lim):
+        for x in range(1, x_lim):
+            red_oldpixel, green_oldpixel, blue_oldpixel = pixel[x, y]
+
+            red_newpixel = apply_threshold(red_oldpixel)
+            green_newpixel = apply_threshold(green_oldpixel)
+            blue_newpixel = apply_threshold(blue_oldpixel)
+
+            pixel[x, y] = red_newpixel, green_newpixel, blue_newpixel
+
+            red_error = red_oldpixel - red_newpixel
+            blue_error = blue_oldpixel - blue_newpixel
+            green_error = green_oldpixel - green_newpixel
+
+            if x < x_lim - 1:
+                red = pixel[x+1, y][0] + round(red_error * 7/16)
+                green = pixel[x+1, y][1] + round(green_error * 7/16)
+                blue = pixel[x+1, y][2] + round(blue_error * 7/16)
+
+                pixel[x+1, y] = (red, green, blue)
+
+            if x > 1 and y < y_lim - 1:
+                red = pixel[x-1, y+1][0] + round(red_error * 3/16)
+                green = pixel[x-1, y+1][1] + round(green_error * 3/16)
+                blue = pixel[x-1, y+1][2] + round(blue_error * 3/16)
+
+                pixel[x-1, y+1] = (red, green, blue)
+
+            if y < y_lim - 1:
+                red = pixel[x, y+1][0] + round(red_error * 5/16)
+                green = pixel[x, y+1][1] + round(green_error * 5/16)
+                blue = pixel[x, y+1][2] + round(blue_error * 5/16)
+
+                pixel[x, y+1] = (red, green, blue)
+
+            if x < x_lim - 1 and y < y_lim - 1:
+                red = pixel[x+1, y+1][0] + round(red_error * 1/16)
+                green = pixel[x+1, y+1][1] + round(green_error * 1/16)
+                blue = pixel[x+1, y+1][2] + round(blue_error * 1/16)
+
+                pixel[x+1, y+1] = (red, green, blue)
+
+    return image_file
